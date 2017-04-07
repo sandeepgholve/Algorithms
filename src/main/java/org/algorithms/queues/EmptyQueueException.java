2eeaf1e9ea58fa00a2886cc58b5803d5e2d82d90
Sandeep Gholve <sandeep.gholve@profitbricks.com>
2017-04-07 20:21:30 +0530

    Implementation of FIFO Queue.

---
+++ src/main/java/org/algorithms/queues/EmptyQueueException.java
@@ -0,0 +1,7 @@
+package org.algorithms.queues;
+
+/**
+ * Created by sgholve on 7/4/17.
+ */
+public class EmptyQueueException extends RuntimeException {
+}
---
+++ src/test/java/org/algorithms/queues/ListFifoQueueTest.java
@@ -0,0 +1,10 @@
+package org.algorithms.queues;
+
+/**
+ * Created by sgholve on 7/4/17.
+ */
+public class ListFifoQueueTest extends AbstractFifoQueueTest {
+    protected Queue createFifoQueue() {
+        return new ListFifoQueue();
+    }
+}
