601e1a4667c96800e1446c5ab01afad0ef78541b
Sandeep Gholve <sandeep.gholve@profitbricks.com>
2017-04-10 19:51:45 +0530

    Implementation of NaturalComparator and ReverseComparator.

---
+++ src/main/java/org/algorithms/sorting/ReverseComparator.java
@@ -0,0 +1,18 @@
+package org.algorithms.sorting;
+
+import java.util.Comparator;
+
+/**
+ * Created by sgholve on 10/4/17.
+ */
+public class ReverseComparator implements Comparator {
+    public static final ReverseComparator INSTANCE = new ReverseComparator();
+
+    private ReverseComparator() {
+    }
+
+    public int compare(Object left, Object right) {
+        assert right != null : "right object can't be null";
+        return ((Comparable) right).compareTo(left);
+    }
+}
